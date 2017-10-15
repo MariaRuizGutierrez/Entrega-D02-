f4449ef0172db9386e983d04fc5c0b6a22a7986b
joaquinperez94 <blogdeaula78@gmail.com>
2017-10-15 19:33:54 +0200

    Cambios MessageFolder, Note

--- Proyecto Java/Acme-Explorer/src/main/java/domain/MessageFolder.java
+++ Proyecto Java/Acme-Explorer/src/main/java/domain/MessageFolder.java
@@ -6,15 +6,15 @@ import org.hibernate.validator.constraints.NotBlank;
 public class MessageFolder extends DomainEntity {
 
 	private String	name;
-	private boolean	modificable;
+	private boolean	modifiable;
 
 
 	public boolean isModificable() {
-		return this.modificable;
+		return this.modifiable;
 	}
 
-	public void setModificable(final boolean modificable) {
-		this.modificable = modificable;
+	public void setModificable(final boolean modifiable) {
+		this.modifiable = modifiable;
 	}
 
 	@NotBlank
