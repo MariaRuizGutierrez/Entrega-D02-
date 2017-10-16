dfeff1b7fd704337a8c0df462f2ee3f72851d35b
joaquinperez94 <blogdeaula78@gmail.com>
2017-10-16 13:42:20 +0200

    Stage

--- Proyecto Java/Acme-Explorer/src/main/java/domain/Stage.java
+++ Proyecto Java/Acme-Explorer/src/main/java/domain/Stage.java
@@ -1,40 +1,56 @@
 
 package domain;
 
+import javax.validation.constraints.NotNull;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class Stage extends DomainEntity {
 
 	private String		title;
 	private String		description;
-	private Double		price;
+	private double		price;
+	private double		iva;
 
 
-
+	@NotNull
 	@NotBlank
 	public String getTitle() {
 		return this.title;
 	}
-
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	@NotNull
 	@NotBlank
 	public String getDescription() {
 		return this.description;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
-	public Double getPrice() {
+	
+	public double getPrice() {
 		return this.price;
 	}
 	
-	public void setPrice(Double price){
+	public void setPrice(double price){
 		this.price=price;
+	}
+
+	public double getIva() {
+		return iva;
+	}
+
+	public void setIva(double iva) {
+		this.iva = iva;
+	}
+	
+	public double getTotalPrice(){
+		return this.price + (this.price*this.iva);
 	}
 
 }
